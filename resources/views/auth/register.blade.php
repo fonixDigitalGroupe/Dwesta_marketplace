56754d02655981c8ee972dd02762fb7fda0d6856
Utilisateur <utilisateur@exemple.com>
2026-06-09 18:21:06 +0000

    style: move breadcrumbs up on register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
         .auth-wrapper {
             max-width: 1200px;
             margin: 0 auto;
-            padding: 2.5rem 1.5rem;
+            padding: 1rem 1.5rem;
             display: flex;
             flex-direction: column;
             align-items: flex-start;
