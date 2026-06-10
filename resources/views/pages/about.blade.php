4bbe0fd90af1652a21f7e7c37d03737a83bc3cd5
Utilisateur <utilisateur@exemple.com>
2026-06-10 19:58:09 +0000

    feat(about): final hero image with correct orange branding

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -508,7 +508,7 @@
     /* Hero Section */
     .about-hero {
         height: 400px;
-        background: url('/images/about_hero_authentic.png') center/cover no-repeat;
+        background: url('/images/about_hero_final.png') center/cover no-repeat;
         display: flex;
         align-items: center;
         position: relative;
