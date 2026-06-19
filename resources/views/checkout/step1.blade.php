1db1f79a9c0b9f74ab4ac5a20c7c9bb5e5d5f741
Utilisateur <utilisateur@exemple.com>
2026-06-19 17:54:57 +0000

    style: match checkout background color with cart page

--- resources/views/checkout/step1.blade.php
+++ resources/views/checkout/step1.blade.php
@@ -7,7 +7,7 @@
         :root {
             --jumia-orange: #f68b1e;
             --jumia-blue: #004aad;
-            --jumia-bg: #ffffff;
+            --jumia-bg: #f5f5f5;
             --jumia-border: #ededed;
             --jumia-text: #313133;
             --jumia-success: #4caf50;
