6725afba41541675ab48c31e684b43405d3ef557
Utilisateur <utilisateur@exemple.com>
2026-06-23 16:08:38 +0000

    Remove 'en tant que particulier' from header dropdown

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -51,11 +51,11 @@
                     </button>
                     <div class="sell-dropdown" id="layoutSellDropdown">
                         <a href="{{ route('annonces.create') }}" class="sell-dropdown-item">
-                            <div class="sell-dropdown-item-title">Vendre un produit en tant que particulier</div>
+                            <div class="sell-dropdown-item-title">Vendre un produit</div>
                             <div class="sell-dropdown-item-subtitle">Je dépose une annonce gratuitement</div>
                         </a>
                         <a href="{{ route('vendeur.astuces') }}" class="sell-dropdown-item" style="padding-top: 0.25rem;">
-                            <div class="sell-dropdown-item-title" style="font-weight: 400; text-decoration: underline;">Astuces vendeurs particuliers</div>
+                            <div class="sell-dropdown-item-title" style="font-weight: 400; text-decoration: underline;">Astuces vendeurs</div>
                         </a>
                         <div class="sell-dropdown-separator"></div>
                         <a href="{{ route('eshop.landing') }}" class="sell-dropdown-item">
